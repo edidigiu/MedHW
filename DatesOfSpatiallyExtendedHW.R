abdf94f875b7ad0b4895a1ddad6fbb5d984e80f1
edidigiu <edmondo@MacBook-Air-di-Edmondo.local>
2016-05-13 17:48:47 +0200

    saving Rproj

--- DatesOfSpatiallyExtendedHW.R
+++ DatesOfSpatiallyExtendedHW.R
@@ -5,7 +5,8 @@
 #
 # 1) starts from t?_ECAD_Duration_HeatWave_Cut1_Extended_Marginal.nc files;
 # 2) moving averages a 10 termini;
-# 3) identifies the whole relative maximum and their dates---new file for Marco
+# 3) from daily to hourly time step using linear regression
+# 4) identifies the whole relative maximum and their dates---new file for Marco
 #------------------------------------------------------------------------------
 
 library(ncdf4)
@@ -13,7 +14,19 @@ library(ncdf4)
 ### FUNCTIONs HUB         ###
 #############################
 # Function for relative maximum::
-
+derivative <- function(f, x, ..., order = 1, delta = 0.1, sig = 6) {
+  # Numerically computes the specified order derivative of f at x
+  vals <- matrix(NA, nrow = order + 1, ncol = order + 1)
+  grid <- seq(x - delta/2, x + delta/2, length.out = order + 1)
+  vals[1, ] <- sapply(grid, f, ...) - f(x, ...)
+  for (i in 2:(order + 1)) {
+    for (j in 1:(order - i + 2)) {
+      stepsize <- grid[i + j - 1] - grid[i + j - 2]
+      vals[i, j] <- (vals[i - 1, j + 1] - vals[i - 1, j])/stepsize
+    }
+  }
+  return(signif(vals[order + 1, 1], sig))
+}
 
 
 #------------------------------
@@ -41,13 +54,41 @@ for (var in c("tn","tx")){
     
     summary(Marginal)
     
-    Marginal.ts<-ts(Marginal,start=c(1951,1),frequency=365)  #seq(timedate,by="day",length.out=nt))
-    plot(Marginal.ts,col="red")
-    require(raster)
-    appo<-movingFun(Marginal.ts,n=10,fun=mean)
+    # Marginal.ts<-ts(Marginal,start=c(1951,1),frequency=365)  #seq(timedate,by="day",length.out=nt))
+    # plot(Marginal.ts,col="red")
+    # require(raster)
+    # appo<-movingFun(Marginal.ts,n=10,fun=mean)
     require(zoo)
-    x<-zoo(Marginal,seq(timedate,by="day",length.out=nt))
-    x.ma<-rollmean(x,10)
-    plot(x.ma) 
+    MargZoo<-zoo(Marginal,seq(timedate,by="day",length.out=nt))
+    x<-rollmean(MargZoo,10)
+    plot(x) 
+    
+    table(cut(x, quantile(x)))
+    
+    RelMax<-function(x)
+    {
+      # x2<-rep(0,length(x))
+      # for(i in 1:length(x))
+      # {
+      #   x2[which.max(x)]<-1  
+      #   x[which.max(x)] <-0
+      #   
+      # }
+      xlag1<-diff(x,1)
+      tutti<-cbind(x,xlag1,xlag1/24)
+      daily<-expand.grid(x=x,y=rep(xlag1/24,each=24))
+      library(pspline)
+      time=time(x)
+      d1<-predict(sm.spline(time, x), time, 1)
+      d2<-predict(sm.spline(time, x), time, 2)
+      appo<-x[d1>-0.5 & d1<0.5 & d2<(-0.5)]
+      plot(appo)
+      abline(v=as.vector(time(appo)),col="lightgrey")
+       
+      
+      dx<-D(x~time,"x")
+      d1<-eval(dx)
+    }
+    
    }
 }
